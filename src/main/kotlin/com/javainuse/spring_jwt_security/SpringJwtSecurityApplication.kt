711d1fdeb0baea7fbe22383c76290a0bab95f8a8
Gabriel Shanahan <GabrielShanahan@gmail.com>
2020-04-12 18:30:25 +0200

    Initial commit

---
+++ src/main/kotlin/com/javainuse/spring_jwt_security/SpringJwtSecurityApplication.kt
@@ -0,0 +1,11 @@
+package com.javainuse.spring_jwt_security
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class SpringJwtSecurityApplication
+
+fun main(args: Array<String>) {
+	runApplication<SpringJwtSecurityApplication>(*args)
+}
---
+++ src/test/kotlin/com/javainuse/spring_jwt_security/SpringJwtSecurityApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.javainuse.spring_jwt_security
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class SpringJwtSecurityApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
